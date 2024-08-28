5d57504e24e3ea4ec1f818bef6bb679bf9a6071b
atallah3 <aratallah3@gmail.com>
2024-08-28 12:38:32 +0300

    init commit

---
+++ SpeedoTransfer/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  SpeedoTransfer
+//
+//  Created by Abd Elrahman Atallah on 27/08/2024.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("kjadslgnk")
+        for family in UIFont.familyNames.sorted() {
+            let names = UIFont.fontNames(forFamilyName: family)
+            print("Family: \(family) Font names: \(names)")
+        }
+    }
+}
