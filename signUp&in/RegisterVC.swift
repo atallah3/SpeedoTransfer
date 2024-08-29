8620bfbfda4700d8370bc8f93db21cc0e2d31017
abdullah <abdullahsilva653@gmail.com>
2024-08-29 05:10:13 -0700

    adding sign up screen added font inter fill sign up screen

---
+++ signUp&in/RegisterVC.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterVC.swift
+//  SpeedoTransfer
+//
+//  Created by Abdullah Silva on 28/08/2024.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sign Up"
+    }
+}
---
+++ signUp&in/login.swift
@@ -0,0 +1,19 @@
+//
+//  login.swift
+//  SpeedoTransfer
+//
+//  Created by Abdullah Silva on 29/08/2024.
+//
+
+import UIKit
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
