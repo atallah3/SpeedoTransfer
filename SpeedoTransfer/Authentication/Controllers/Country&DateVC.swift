2f7a98261b0218772f41672b7216bd7e7773b0b4
abdullah <abdullahsilva653@gmail.com>
2024-08-29 22:58:46 -0700

    update UI for (RegistrationVC , login VC) created Country&DateVC Update assest

---
+++ SpeedoTransfer/Authentication/Controllers/Country&DateVC.swift
@@ -0,0 +1,30 @@
+//
+//  Country&DateVC.swift
+//  SpeedoTransfer
+//
+//  Created by Abdullah Silva on 29/08/2024.
+//
+
+import UIKit
+
+class Country_DateVC: UIViewController {
+
+    @IBOutlet weak var CountrySelect: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CountrySelect.layer.borderWidth = 1
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
