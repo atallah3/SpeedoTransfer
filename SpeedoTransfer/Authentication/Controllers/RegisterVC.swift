6cf5b682b0e36ddacb93352fb5b4279380cd8b4c
atallah3 <aratallah3@gmail.com>
2024-08-30 16:26:54 +0300

    Add constrains to Authentication module

--- SpeedoTransfer/Authentication/Controllers/RegisterVC.swift
+++ SpeedoTransfer/Authentication/Controllers/RegisterVC.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class RegisterVC: UIViewController {
-    // MARK: - Connections
+    
+    // MARK: - IBOutlet
     @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -18,32 +19,17 @@ class RegisterVC: UIViewController {
     
     private var isPasswordVisible = false
     private var isConfirmPasswordVisible = false
-//MARK: button Sign UP
-    @IBAction func SignUpBtn(_ sender: UIButton) {
-        guard let name = fullNameTextField.text?.trimmed,  let email = emailTextField.text?.trimmed, let password = passwordTextField.text?.trimmed, let confirmPassword = confirmPasswordTextField.text?.trimmed, password == confirmPassword
-        else {return}
-        
-        let user: User = User(name: name, email: email, password: password, confirmPassword: confirmPassword)
-        let sb = UIStoryboard(name: "Authentication", bundle: nil)
-        let continueToCountryVC = sb.instantiateViewController(withIdentifier: "CountryAndDateVC") as! CountryAndDateVC
-        self.navigationController?.pushViewController(continueToCountryVC, animated: true)
-    }
-
-    @IBAction func signInbtn1(_ sender: UIButton) {
-        let sb = UIStoryboard(name: "Authentication", bundle: nil)
-        let toLogin = sb.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
-        self.navigationController?.pushViewController(toLogin, animated: true)
-    }
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let passwordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(togglePasswordVisibility))
-        passwordEyeImageView.isUserInteractionEnabled = true
-        passwordEyeImageView.addGestureRecognizer(passwordEyeTapGesture)
-
-        let confirmPasswordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(toggleConfirmPasswordVisibility))
-        confirmPasswordEyeImageView.isUserInteractionEnabled = true
-        confirmPasswordEyeImageView.addGestureRecognizer(confirmPasswordEyeTapGesture)
+//        let passwordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(togglePasswordVisibility))
+//        passwordEyeImageView.isUserInteractionEnabled = true
+//        passwordEyeImageView.addGestureRecognizer(passwordEyeTapGesture)
+//
+//        let confirmPasswordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(toggleConfirmPasswordVisibility))
+//        confirmPasswordEyeImageView.isUserInteractionEnabled = true
+//        confirmPasswordEyeImageView.addGestureRecognizer(confirmPasswordEyeTapGesture)
     }
     
     @objc func togglePasswordVisibility() {
@@ -59,4 +45,23 @@ class RegisterVC: UIViewController {
         let eyeImageName = isConfirmPasswordVisible ? "Component 2" : "closedEye"
         confirmPasswordEyeImageView.image = UIImage(named: eyeImageName)
     }
+    
+//MARK: button Sign UP
+    @IBAction func SignUpBtn(_ sender: UIButton) {
+        guard let name = fullNameTextField.text?.trimmed ,  let email = emailTextField.text?.trimmed , let password = passwordTextField.text?.trimmed, let confirmPassword = confirmPasswordTextField.text?.trimmed, password == confirmPassword
+        else { return }
+        
+        let user: User = User(name: name, email: email, password: password)
+        let sb = UIStoryboard(name: "Authentication", bundle: nil)
+        let continueToCountryVC = sb.instantiateViewController(withIdentifier: "CountryAndDateVC") as! CountryAndDateVC
+        self.navigationController?.pushViewController(continueToCountryVC, animated: true)
+    }
+
+    @IBAction func signInbtn1(_ sender: UIButton) {
+        let sb = UIStoryboard(name: "Authentication", bundle: nil)
+        let toLogin = sb.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+        self.navigationController?.pushViewController(toLogin, animated: true)
+    }
+    
+   
 }
--- SpeedoTransfer/Extensions/String+EXT.swift
+++ SpeedoTransfer/Extensions/String+EXT.swift
@@ -1,13 +1,13 @@
 //
-//  trimming.swift
+//  String+EXT.swift
 //  SpeedoTransfer
 //
-//  Created by Abdullah Silva on 30/08/2024.
+//  Created by Abd Elrahman Atallah on 30/08/2024.
 //
 
 import Foundation
 
-extension String{
+extension String {
     var trimmed: String {
         return self.trimmingCharacters(in: .whitespacesAndNewlines)
     }
