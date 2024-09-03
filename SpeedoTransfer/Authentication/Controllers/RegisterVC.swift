f039be9b106a285cbc0f9445359189e587a83ccc
abdullah <abdullahsilva653@gmail.com>
2024-09-03 02:42:06 -0700

    password validation in register

--- SpeedoTransfer/Authentication/Controllers/RegisterVC.swift
+++ SpeedoTransfer/Authentication/Controllers/RegisterVC.swift
@@ -14,40 +14,49 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    @IBOutlet weak var passwordEyeImageView: UIImageView!
-    @IBOutlet weak var confirmPasswordEyeImageView: UIImageView!
+    @IBOutlet weak var errorlabel: UILabel!
     
     private var isPasswordVisible = false
     private var isConfirmPasswordVisible = false
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let passwordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(togglePasswordVisibility))
-//        passwordEyeImageView.isUserInteractionEnabled = true
-//        passwordEyeImageView.addGestureRecognizer(passwordEyeTapGesture)
-//
-//        let confirmPasswordEyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(toggleConfirmPasswordVisibility))
-//        confirmPasswordEyeImageView.isUserInteractionEnabled = true
-//        confirmPasswordEyeImageView.addGestureRecognizer(confirmPasswordEyeTapGesture)
+        errorlabel.isHidden = true
     }
-    
-    @objc func togglePasswordVisibility() {
-        isPasswordVisible.toggle()
-        passwordTextField.isSecureTextEntry = !isPasswordVisible
-        let eyeImageName = isPasswordVisible ? "Component 2" : "closedEye"
-        passwordEyeImageView.image = UIImage(named: eyeImageName)
-    }
-    
-    @objc func toggleConfirmPasswordVisibility() {
-        isConfirmPasswordVisible.toggle()
-        confirmPasswordTextField.isSecureTextEntry = !isConfirmPasswordVisible
-        let eyeImageName = isConfirmPasswordVisible ? "Component 2" : "closedEye"
-        confirmPasswordEyeImageView.image = UIImage(named: eyeImageName)
-    }
-    
+    func validateSignup() {
+          guard let password = passwordTextField.text, isValidPassword(password) else {
+              showPasswordError()
+              return
+          }
+          clearPasswordError()
+      }
+      
+      func isValidPassword(_ password: String) -> Bool {
+          let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[0-9])[A-Za-z\\d]{8,}$")
+          return passwordPredicate.evaluate(with: password)
+      }
+      
+      func showPasswordError() {
+          passwordTextField.layer.borderColor = UIColor.red.cgColor
+          passwordTextField.layer.borderWidth = 1.0
+          confirmPasswordTextField.layer.borderColor = UIColor.red.cgColor
+          confirmPasswordTextField.layer.borderWidth = 1.0
+          errorlabel.text = "Password must be at least 8 characters long and include a number."
+          errorlabel.textColor = UIColor.red
+          errorlabel.isHidden = false
+          UIView.animate(withDuration: 0.3) {
+              self.passwordTextField.layer.borderColor = UIColor.red.cgColor
+          }
+      }
+      func clearPasswordError() {
+          passwordTextField.layer.borderColor = UIColor.clear.cgColor
+          errorlabel.text = ""
+          errorlabel.isHidden = true
+      }
+
 //MARK: button Sign UP
     @IBAction func SignUpBtn(_ sender: UIButton) {
+        
         guard let name = fullNameTextField.text?.trimmed , !name.isEmpty ,  let email = emailTextField.text?.trimmed, !email.isEmpty, let password = passwordTextField.text?.trimmed, !password.isEmpty, let confirmPassword = confirmPasswordTextField.text?.trimmed ,!confirmPassword.isEmpty, password == confirmPassword
         else {return }
         
@@ -55,6 +64,7 @@ class RegisterVC: UIViewController {
         let sb = UIStoryboard(name: "Authentication", bundle: nil)
         let continueToCountryVC = sb.instantiateViewController(withIdentifier: "CountryAndDateVC") as! CountryAndDateVC
         self.navigationController?.pushViewController(continueToCountryVC, animated: true)
+        validateSignup()
     }
 
     @IBAction func signInbtn1(_ sender: UIButton) {
@@ -63,5 +73,6 @@ class RegisterVC: UIViewController {
         self.navigationController?.pushViewController(toLogin, animated: true)
     }
     
+
    
 }
