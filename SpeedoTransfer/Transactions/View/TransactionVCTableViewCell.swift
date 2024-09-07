8a4cae5351ad2287b672592d3a57af36b7a4eb11
abdullah <abdullahsilva653@gmail.com>
2024-09-07 14:37:44 -0700

    add transactions update assists update transactions cell for table view

---
+++ SpeedoTransfer/Transactions/View/TransactionVCTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionVCTableViewCell.swift
+//  SpeedoTransfer
+//
+//  Created by Abdullah Silva on 07/09/2024.
+//
+
+import UIKit
+
+class TransactionVCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
